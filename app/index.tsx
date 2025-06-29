9656ff82af4a37be6857a97a45779d30aba95af2
Kevin Lopez <129701490+KevLopezTech@users.noreply.github.com>
2025-06-29 04:20:18 -0400

    Updated from react-native-iap to expo-iap

--- app/index.tsx
+++ app/index.tsx
@@ -68,7 +68,7 @@ export default function HomeScreen() {
         ? TestIds.BANNER
         : (Platform.OS === 'ios'
             ? 'YOUR_REAL_IOS_AD_UNIT_ID' // Replace with your actual Ad Unit ID
-            : 'ca-app-pub-3213905360913918/6717005440'); // Replace with your actual Ad Unit ID
+            : 'ca-app-pub-3213905360913918/5699853504'); // Replace with your actual Ad Unit ID
 
     const billAmountNum = parseFloat(inputAmount);
 
